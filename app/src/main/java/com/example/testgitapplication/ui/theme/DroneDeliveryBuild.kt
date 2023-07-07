a3e363fd516c57183535c5142309832e9224da5a
Sriharsha G <harsha.g.ece@gmail.com>
2023-07-07 14:25:46 -0400

    DroneDelvery Machine Logic

---
+++ app/src/main/java/com/example/testgitapplication/ui/theme/DroneDeliveryBuild.kt
@@ -0,0 +1,6 @@
+package com.example.testgitapplication.ui.theme
+
+class DroneDeliveryBuild {
+
+    // Logic for drone delivery based on address location
+}
---
+++ app/src/main/java/com/example/testgitapplication/ui/theme/DroneDeliveryMachineLogic.kt
@@ -0,0 +1,4 @@
+package com.example.testgitapplication.ui.theme
+
+class DroneDeliveryMachineLogic {
+}
